944cc2b773e7c26c5fc141f11ac2272245dd4e63
yfsteven <yaninquiry@gmail.com>
2024-01-29 15:43:16 -0500

    Improved Table Images and Added Coordinates to Each Block

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const getPlayerBlocksContainer = document.getElementById('player-blocks-container');
-const getEnemyBlocksContainer = document.getElementById('enemy-blocks-container');
+const playerBlocksContainer = document.getElementById('player-blocks-container');
+const enemyBlocksContainer = document.getElementById('enemy-blocks-container');
 
 const createGrid = (table) => {
+    const letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
     for(let i = 0; i < 10; i++){
         const tr = document.createElement('tr');
         for(let w = 0; w < 10; w++){
             const td = document.createElement('td');
-            tr.append(td);
+            if(i === 0) {
+                td.className = `${letterArray[w]}1`;
+                tr.append(td);
+            } else {
+                td.className = `${letterArray[w]}${w + 1}`;
+                tr.append(td);
+            }
         }
         table.appendChild(tr);
     }
 }
-createGrid(getPlayerBlocksContainer);
-createGrid(getEnemyBlocksContainer);
+createGrid(playerBlocksContainer);
+createGrid(enemyBlocksContainer);
+
+const allBlocks = document.querySelectorAll('.blocks');
+
+allBlocks.forEach((block) => {
+    block.addEventListener('click', () => {
+        console.log(block.outerHTML);
+    });
+});
+
 
 class Ship {
     constructor() {
